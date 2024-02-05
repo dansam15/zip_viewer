6fc212a0f458c2306dfc322e07fd32bb73ce34ec
dansam15 <samirkhanov@frtk.ru>
2024-02-05 21:30:05 +0300

    revert random /dbg dir commit

--- dbg/CMakeFiles/zip_viewer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/zip_viewer_autogen"
-  "zip_viewer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/zip_viewer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
